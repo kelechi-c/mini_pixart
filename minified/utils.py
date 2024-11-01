d703431b172b45b63806775e52c60313f9fc2611
Tensor <narutokelechi@gmail.com>
2024-11-01 12:45:12 +0100

    [modify] update config

--- minified/utils.py
+++ minified/utils.py
@@ -2,30 +2,37 @@ import torch
 from PIL import Image as pillow
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# painting of a man's silhouette ona mountain gazing at the full moon in a colorful night sky
 class config:
     lr = 2e-5
-    data_split = 10_000
+    data_split = 1000
     img_size = 256
     patch_size = 4
+    dtype = torch.float16
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     epochs = 2
     batch_size = 16
+    sample_steps = 1000
+    scale_factor = 0.18215
 
-    # mdodel specs
+    # model specs
     tr_blocks = 12
     freq_embed = 256
     attn_heads = 6
     hidden_size = 768
+    embed_dim = 768
+    latent_size = 32
+    mlp_ratio = 4.0
 
     # dataset/model ids
     dataset_id = "laion/laion2B-en-aesthetic"
+    processed_dataset_id = ''
     sd_vae_id = ''
     pretrained_id = ''
     moondream_id = "vikhyatk/moondream2"
 
 
 moondream_model = AutoModelForCausalLM.from_pretrained(config.moondream_id, trust_remote_code=True)
-
 md_tokenizer = AutoTokenizer.from_pretrained(config.moondream_id, trust_remote_code=True)
 
 
